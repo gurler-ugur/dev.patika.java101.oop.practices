9be9fbfbb7de9b9c627a5e641a0c1c3d79390a07
Ugur Gurler <ugur.gurler@mezun.deu.edu.tr>
2023-01-08 22:07:38 +0300

    FindMatrixTranspose.java class is added.

---
+++ dev.patika/Java/Java101OOP/src/ArraysPractices/FindMatrixTranspose.java
@@ -0,0 +1,51 @@
+package ArraysPractices;
+
+import java.util.Scanner;
+
+public class FindMatrixTranspose {
+    public static void main(String[] args) {
+
+        int[][] matrix = createMatrix(2,3);
+        printMatrix(matrix);
+        int[][] transposeMatrix = transposeOfMatrix(matrix);
+        printMatrix(transposeMatrix);
+
+    }
+
+    public static int[][] createMatrix(int row, int column) {
+        int[][] matrix = new int[row][column];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = createRandomNumber1To9();
+            }
+        }
+        return matrix;
+    }
+
+    public static int[][] transposeOfMatrix(int[][] matrix) {
+        int columnTranspose = matrix.length;
+        int rowTranspose = matrix[0].length;
+        int[][] transpose = new int[rowTranspose][columnTranspose];
+
+        for (int i = 0, k = 0; i < transpose.length; i++, k++) {
+            for (int j = 0, l = 0; j < transpose[i].length; j++, l++) {
+                transpose[i][j] = matrix[l][k];
+            }
+        }
+        return transpose;
+    }
+
+    public static int createRandomNumber1To9() {
+        return  (int)(Math.random() * 10);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
+    }
+}
